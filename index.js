e6810b75cc99882c8fee41e3d248ad7c633ff9f5
sakibsarkar <sakibsarkar707@gmail.com>
2024-01-12 00:04:45 +0600

    category bassed food api

--- index.js
+++ index.js
@@ -2,10 +2,21 @@ require('dotenv').config()
 const express = require("express")
 const port = process.env.PORT || 5000
 const app = express()
+const cors = require("cors")
+const cookieParser = require("cookie-parser")
+
+app.use(express.json())
+app.use(cors({
+    origin: ["http://localhost:5173"],
+    credentials: true
+}))
+
+app.use(cookieParser())
+
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.MONGO_USER}:<password>@cluster0.xbiw867.mongodb.net/?retryWrites=true&w=majority`;
-
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.xbiw867.mongodb.net/?retryWrites=true&w=majority`;
+console.log(process.env.MONGO_USER);
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
@@ -15,12 +26,36 @@ const client = new MongoClient(uri, {
     }
 });
 
+// collection
+const foodCollection = client.db("FoodEx").collection("foodCollection")
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
         // Send a ping to confirm a successful connection
         // await client.db("admin").command({ ping: 1 });
+
+
+        // ------food related api--------
+
+        // category based product
+        app.get("/api/food/", async (req, res) => {
+            const category = req.query.category;
+            const limit = req.query.limit;
+            const find = { category: category };
+            if (limit) {
+                const result = await foodCollection.find(find).limit(parseInt(limit)).toArray();
+                return res.send(result);
+            }
+            const result = await foodCollection.find(find).toArray();
+            return res.send(result);
+        })
+
+
+
+
+
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
